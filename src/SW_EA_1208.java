bc1087e93cfb8226a10f60f5446a927865cd61ce
sang <mim3431@naver.com>
2018-03-06 07:28:41 +0900

    삼성 알고리즘 1208번문제

---
+++ src/SW_EA_1208.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class SW_EA_1208 {
+
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int num, answer, max, min;
+
+        int lengths[] = new int[101];
+
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            num = sc.nextInt();
+
+            Arrays.fill(lengths, 0);
+            for (int i = 0; i < 100; i++) {
+                lengths[sc.nextInt()]++;
+            }
+
+            min = 1;
+            while (lengths[min] == 0) min++;
+            max = 100;
+            while (lengths[max] == 0) max--;
+
+            for (int i = 0; i < num; i++) {
+                if (max - 1 > min) {
+                    lengths[max]--;
+                    lengths[max-1]++;
+                    lengths[min]--;
+                    lengths[min+1]++;
+
+                    if (lengths[max] <= 0) max -= 1;
+                    if (lengths[min] <= 0) min += 1;
+                } else break;
+            }
+
+            answer = max - min;
+            System.out.println(String.format("#%d %d", test_case, answer));
+        }
+    }
+
+}
+
+// https://www.swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV139KOaABgCFAYh&categoryId=AV139KOaABgCFAYh&categoryType=CODE
