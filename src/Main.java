2a17d5612aacdaf9c992a63c0bcea9b8e6f9f8d5
sang <mim3431@naver.com>
2018-08-31 11:12:19 +0900

    Main 기본구성 변경

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    public static void main(String args[]) throws Exception
-    {
-        Scanner sc = new Scanner(System.in);
-        int T;
-        T=sc.nextInt();
+    static int T;
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st= new StringTokenizer(br.readLine());
+        T = Integer.parseInt(st.nextToken());
 
         for (int test_case = 1; test_case <= T; test_case++) {
-
-            System.out.println(String.format("#%d %d", test_case, 1));
+            bw.write(String.format("#%d %d", test_case, 1));
         }
+        bw.flush();
     }
-
 }
